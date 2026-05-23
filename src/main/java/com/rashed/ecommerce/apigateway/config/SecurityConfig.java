717adcd1903f899112c184c7e5af45a00c4cdfa6
mohammed-rashed <jstrasshed@gmail.com>
2026-05-23 17:00:13 +0300

    add security configuration and JWT support for API Gateway

---
+++ src/main/java/com/rashed/ecommerce/apigateway/config/SecurityConfig.java
@@ -0,0 +1,51 @@
+package com.rashed.ecommerce.apigateway.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+
+@Configuration
+public class SecurityConfig {
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        return http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(exchange -> exchange
+                        .pathMatchers("/actuator/**").permitAll()
+                        .pathMatchers("/api/v1/auth/**").permitAll()
+
+                        .pathMatchers("/api/v1/orders", "/api/v1/orders/**").authenticated()
+                        .pathMatchers("/api/v1/products", "/api/v1/products/**").authenticated()
+                        .pathMatchers("/api/v1/categories", "/api/v1/categories/**").authenticated()
+                        .pathMatchers("/api/v1/inventory", "/api/v1/inventory/**").authenticated()
+
+                        .anyExchange().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+                .build();
+    }
+
+    @Bean
+    public ReactiveJwtDecoder reactiveJwtDecoder() {
+        SecretKey secretKey = new SecretKeySpec(
+                jwtSecret.getBytes(StandardCharsets.UTF_8),
+                "HmacSHA384"
+        );
+
+        return NimbusReactiveJwtDecoder.withSecretKey(secretKey)
+                .macAlgorithm(MacAlgorithm.HS384)
+                .build();
+    }
+}
